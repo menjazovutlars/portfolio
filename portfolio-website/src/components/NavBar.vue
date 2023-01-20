54c1f3fe1e564a204c23ce08d2ff553705a3baba
larsibarsi <58939896+menjazovutlars@users.noreply.github.com>
2023-01-20 17:59:52 +0100

    first commit
    Issues: Let buttons appear after specific paragraph is visible. recognize answer and let them dissapear again

---
+++ portfolio-website/src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template lang="">
+  <v-app-bar app color="#1111111" dark flat class="px-8">
+    <v-btn icon>
+      <v-icon color="#A7121D">fas fa-dragon</v-icon>
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn text>Home</v-btn>
+    <v-btn text>About</v-btn>
+    <v-btn text>Projects</v-btn>
+    <v-btn text>Contact</v-btn>
+  </v-app-bar>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
---
+++ portfolio-website/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app :style="{ background: $vuetify.theme.themes.dark.background }">
+    <NavBar />
+    <StartPage />
+  </v-app>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+import StartPage from "@/components/StartPage.vue";
+
+export default {
+  name: "HomePage",
+
+  components: {
+    NavBar,
+    StartPage,
+  },
+};
+</script>
